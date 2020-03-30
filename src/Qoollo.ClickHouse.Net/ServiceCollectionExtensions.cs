03f4034a4411f449347cd25263fd67dbe45b4b69
Святослав Сычев <rk474@yandex.ru>
2020-03-30 17:40:39 +0300

    add provider extention

---
+++ src/Qoollo.ClickHouse.Net/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Qoollo.ClickHouse.Net.Configuration;
+using Qoollo.ClickHouse.Net.ConnectionPool;
+using Qoollo.ClickHouse.Net.Repository;
+
+namespace Qoollo.ClickHouse.Net
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddClickHouseRepository(this IServiceCollection services, IConfigurationSection configuration)
+        {
+            var config = configuration.Get<ClickHouseConfiguration>();
+            services.AddTransient<IClickHouseConfiguration>(serviceProvider => config);
+
+            services.AddSingleton(serviceProvider => new ClickHouseConnectionPool(
+                config: config,
+                name: "ClickHouseConnectionPool",
+                logger: serviceProvider.GetRequiredService<ILogger>()));
+
+            services.AddTransient<IClickHouseRepository, ClickHouseRepository>();
+        }
+    }
+}
