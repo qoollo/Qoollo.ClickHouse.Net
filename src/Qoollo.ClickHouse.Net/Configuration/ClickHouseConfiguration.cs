ef3718fe49ae4df303c766c97616ecd9b69894ae
Святослав Сычев <rk474@yandex.ru>
2020-04-02 16:04:55 +0300

    #2 add coments for config

--- src/Qoollo.ClickHouse.Net/Configuration/ClickHouseConfiguration.cs
+++ src/Qoollo.ClickHouse.Net/Configuration/ClickHouseConfiguration.cs
@@ -2,10 +2,19 @@
 
 namespace Qoollo.ClickHouse.Net.Configuration
 {
+    /// <summary>
+    /// Configuration for ClickHouseConnectionPool
+    /// </summary>
     public class ClickHouseConfiguration : IClickHouseConfiguration
     {
+        /// <summary>
+        /// List of connection strings.
+        /// </summary>
         public List<string> ConnectionStrings { get; set; }
 
+        /// <summary>
+        /// Maximum elements count for connection pool.
+        /// </summary>
         public int ConnectionPoolMaxCount { get; set; }
     }
 }
