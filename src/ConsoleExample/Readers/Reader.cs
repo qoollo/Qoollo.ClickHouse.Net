04982221bb6ab2b8992fe5007e87abc6cb57ca8e
Святослав Сычев <rk474@yandex.ru>
2020-04-02 21:03:36 +0300

    #6 add console test app

---
+++ src/ConsoleExample/Readers/Reader.cs
@@ -0,0 +1,35 @@
+﻿using ClickHouse.Ado;
+using Qoollo.ClickHouse.Net.Repository;
+using System.Collections.Generic;
+
+namespace ConsoleExample.Readers
+{
+    public class Reader : IReader
+    {
+        private readonly IClickHouseRepository _clickHouseRepository;
+
+        private readonly string _selectByUserIdQuery = $"SELECT * FROM {Entity.TableName} WHERE userId = @userId";
+
+        public Reader(IClickHouseRepository clickHouseRepository)
+        {
+            _clickHouseRepository = clickHouseRepository;
+        }
+
+        public IEnumerable<Entity> SelectEntitiesByUserId(long userId)
+        {
+            var mapper = new EntityMapper();
+
+            var parameters = new List<ClickHouseParameter>()
+            {
+                new ClickHouseParameter()
+                {
+                    ParameterName = "userId",
+                    Value = userId,
+                    DbType = System.Data.DbType.Int64
+                }
+            };
+
+            return _clickHouseRepository.ExecuteQueryMapping(_selectByUserIdQuery, mapper, parameters);
+        }
+    }
+}
